a045730ad7b57aab10b595585cc8c6860673efe0
kevin_p <kprsd14@gmail.com>
2022-01-18 22:04:44 -0500

    added score button functionality

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const avatar = document.querySelector('.avatar'),
       plusButton = document.getElementById('plus-button'),
       minusButton = document.getElementById('minus-button'),
       replyButton = document.querySelector('.reply-btn'),
+      firstComment = document.getElementById('one'),
+      input = document.querySelector('.comment-input'),
       container = document.querySelector('.container');
 
       let scoreNumberConverted = parseInt(scoreNumber.innerText);
@@ -74,10 +76,62 @@ const avatar = document.querySelector('.avatar'),
 
 // getData();
 
+// Score button functions
+
 const addScore = () => {
-
-  scoreNumberConverted ++;
-  console.log(scoreNumberConverted);
-
+  scoreNumberConverted++;
+  scoreNumber.innerText = scoreNumberConverted;
 }
+
+const subtractScore = () => {
+  scoreNumberConverted--;
+  scoreNumber.innerText = scoreNumberConverted;
+}
+
+const generateReply = () => {
+
+const newComment = document.createElement('div');
+newComment.classList.add('comment-reply');
+newComment.innerHTML = `
+<div class="comment-reply">
+<div class="comment-card comment-child">
+  <div class="comment-card-header">
+    <img class="avatar" src="images/avatars/image-amyrobson.png" />
+    <span class="name">amyrobson</span>
+    <span class="time-created">1 month ago</span>
+  </div>
+  <p class="comment-content">
+    Impressive! Though it seems the drag feature could be improved. But
+    overall it looks incredible. You've nailed the design and the
+    responsiveness at various breakpoints works really well.
+  </p>
+  <div class="card-bottom">
+    <div class="score">
+      <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
+          fill="#C5C6EF"
+          id="minus"
+          class="score-button"
+        />
+      </svg>
+      <span class="number">12</span>
+      <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
+          fill="#C5C6EF"
+          id="plus"
+          class="score-button"
+        />
+      </svg>
+    </div>
+  </div>
+</div>
+</div>
+
+`
+container.appendChild(newComment);
+}
+
+replyButton.addEventListener('click', generateReply);
 
